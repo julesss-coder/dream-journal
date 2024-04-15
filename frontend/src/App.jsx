816f15d9ef26cde9cbef538bdc83b8ce76207971
julesss-coder <julesss@posteo.de>
2024-04-15 16:22:21 +0200

    Add drawer. Render dream titles in drawer.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,10 +5,21 @@ import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 import BubbleChartIcon from '@mui/icons-material/BubbleChart';
+import IconButton from '@mui/joy/IconButton';
+import Drawer from '@mui/joy/Drawer';
+import ModalClose from '@mui/joy/ModalClose';
+import Menu from '@mui/icons-material/Menu';
+import Input from '@mui/joy/Input';
+import List from '@mui/joy/List';
+import ListItemButton from '@mui/joy/ListItemButton';
+import Search from '@mui/icons-material/Search';
+
+
 
 
 function App() {
   const [dreams, setDreams] = useState(null);
+  const [open, setOpen] = React.useState(false);
 
   useEffect(() => {
     const fetchDreams = async () => {
@@ -33,17 +44,103 @@ function App() {
   return (
     <Box 
       component="main"
+      variant="outlined" 
       sx={{
         display: "flex",
         flexDirection: "column",
         height: "100vh",
         width: "100%",
-        bgcolor: "lightgray",
+        // bgcolor: "lightgray",
       }}  
     >
-      <Typography>
-        Test
-      </Typography>
+      <IconButton 
+        variant="outlined" 
+        color="neutral" 
+        onClick={() => setOpen(true)}
+        sx={{
+          justifyContent: "flex-start"
+        }}  
+      >
+        <Menu />
+      </IconButton>
+      <Drawer 
+        open={open} 
+        onClose={() => setOpen(false)}
+        color="primary"
+        invertedColors
+        size="sm"
+        variant="soft"  
+      >
+        <Box
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            gap: 0.5,
+            ml: 'auto',
+            mt: 1,
+            mr: 2,
+          }}
+        >
+          <Typography
+            component="label"
+            htmlFor="close-icon"
+            fontSize="sm"
+            fontWeight="lg"
+            sx={{ cursor: 'pointer' }}
+          >
+            Close
+          </Typography>
+          <ModalClose id="close-icon" sx={{ position: 'initial' }} />
+        </Box>
+        <Input
+          size="sm"
+          placeholder="Search"
+          variant="plain"
+          endDecorator={<Search />}
+          slotProps={{
+            input: {
+              'aria-label': 'Search anything',
+            },
+          }}
+          sx={{
+            m: 3,
+            borderRadius: 0,
+            borderBottom: '2px solid',
+            borderColor: 'neutral.outlinedBorder',
+            '&:hover': {
+              borderColor: 'neutral.outlinedHoverBorder',
+            },
+            '&::before': {
+              border: '1px solid var(--Input-focusedHighlight)',
+              transform: 'scaleX(0)',
+              left: 0,
+              right: 0,
+              bottom: '-2px',
+              top: 'unset',
+              transition: 'transform .15s cubic-bezier(0.1,0.9,0.2,1)',
+              borderRadius: 0,
+            },
+            '&:focus-within::before': {
+              transform: 'scaleX(1)',
+            },
+          }}
+        />
+        <List
+          size="lg"
+          component="nav"
+          sx={{
+            flex: 'none',
+            fontSize: 'xl',
+            '& > div': { justifyContent: 'flex-start' },
+          }}
+        >
+          {
+            dreams && dreams.map(dream => (
+              <ListItemButton>{dream.title}</ListItemButton>
+            ))
+          }
+        </List>
+      </Drawer>
       <BottomNavigation
         showLabels
         sx={{
