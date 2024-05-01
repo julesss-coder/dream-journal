9f83d11e1ace58b4d06a62603eb2b632675f7801
julesss-coder <julesss@posteo.de>
2024-05-01 18:19:10 +0200

    Check or create database 'dreams' and three tables

--- backend/server.js
+++ backend/server.js
@@ -4,51 +4,20 @@ users: root, dreamjournaluser - different password
 identification methods, passwords, etc. need to be configured for each user separately!!!
 */
 
-/*
-set up server
-if database does not exist:
-  create database dreams
-
-*/
-
-// const http = require('http');
-// const port = 8000;
-// const server = http.createServer((request, response) => {
-//   response.writeHead(200, {"Content-Type": "text"});
-//   response.end("Hello, world!");
-// });
-
-// server.listen(port);
 require('dotenv').config();
 const mySQL = require('mysql');
 const express = require('express');
 const app = express();
 const port = 8000;
+const util = require('util');
 
 const connection = mySQL.createConnection({
-  // host: process.env.DB_HOST,
-  // user: process.env.DB_USER,
-  // database: process.env.DB_NAME,
-  // password: process.env.DB_PASSWORD
   host: 'localhost',
   user: 'dreamjournaluser',
   password: process.env.DB_PASSWORD
-//   DB_HOST=localhost
-// DB_USER=dreamjournaluser
-// DB_PASSWORD=Password123!
-// DB_NAME=dreams
 });
 
-// connection.connect((error) => {
-//   if (error) throw error;
-//   console.log('Connected to MySQL');
-
-//   connection.query('SHOW DATABASES', (error, result) => {
-//     if (error) throw error;
-
-//     console.log('Databases: ', result);
-//   });
-// });
+const query = util.promisify(connection.query).bind(connection);
 
 connection.connect((error) => {
   if (error) {
@@ -56,14 +25,57 @@ connection.connect((error) => {
     return;
   }
   console.log('Connected to MySQL');
+
+  query("SHOW DATABASES LIKE 'dreams'")
+  .then((result) => {
+    if (result.length > 0) {
+      console.log("Database dreams exists.");
+      return query('USE dreams');
+    } else {
+      console.log("Database 'dreams' does not exist");
+      throw new Error("Database 'dreams' does not exist");
+    }
+  })
+  .then(() => {
+    return query(`CREATE TABLE IF NOT EXISTS dreamlog (
+      id INT PRIMARY KEY, 
+      userid INT, 
+      title VARCHAR(255), 
+      description TEXT, 
+      thoughts TEXT, 
+      dateCreated DATETIME, 
+      lastEdited DATETIME
+    )`);
+  })
+  .then(() => {
+    console.log("Table `dreamlog` created, or already exists.");
+    return query(`CREATE TABLE IF NOT EXISTS tags (
+      id INT PRIMARY KEY, 
+      tag VARCHAR(255), 
+      UNIQUE (tag)
+    )`);
+  })
+  .then(() => {
+    console.log("Table `tags` created, or already exists.");
+    return query(`CREATE TABLE IF NOT EXISTS dreams_tags (
+      dream_id INT,
+      tag_id INT,
+      PRIMARY KEY (dream_id, tag_id),
+      FOREIGN KEY (dream_id) REFERENCES dreamlog(id),
+      FOREIGN KEY (tag_id) REFERENCES tags(id)
+    )`);
+  })
+  .then(() => {
+    console.log("Table `drams_tags` created, or already exists.");
+  })
+  .catch((error) => {
+    throw error;
+  });
 });
 
 // Routes
 app.get('/', (request, response) => {
   console.log("request: ", request.method, request.url);
-  // console.log("request.headers: ", request.headers);
-  // console.log("request.body: ", request.body);
-  // console.log("response: ", response); // (undefined, 2%) (huge object)
   response.send("Hello, world!");
 });
 
